c4ea382dade86a74390e69f21f066591f3902dec
AndreyParayko <32190537+AndreyParayko@users.noreply.github.com>
2021-10-27 22:55:45 +0300

    update client api

--- src/api/books.js
+++ src/api/books.js
@@ -1,5 +1,5 @@
 import client from "./client";
 
-export const getBooks = () => client.get('/Books')
-export const getBookById = (id) => client.get(`/Books/${id}`)
+export const getBooks = () => client.get('/books')
+export const getBookById = (id) => client.get(`/books/${id}`)
 
